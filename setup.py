7764014516421c88a8bc1c44b4bb6a342bcbf981
odette-elementl <122291097+odette-elementl@users.noreply.github.com>
2023-02-02 15:12:19 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,16 +4,15 @@ setup(
     name="airflow-dagster-migration",
     packages=find_packages(),
     install_requires=[
-        "dagster",
-        "dagster-cloud",
-        "dagster_airflow>=0.17.5",
+        "dagster>=1.0.0",
+        "dagster_cloud>=1.0.0",
+        "dagster_airflow>=0.17.7",
         "apache-airflow==2.5.0",
         # airflow dag specific dependencies
         "docker>=5.0.3,<6.0.0",
         "apache-airflow-providers-docker>=3.2.0,<4",
         "apache-airflow-providers-apache-spark>=3.0.0,<4",
         "kubernetes>=10.0.1",
-
     ],
     extras_require={"dev": ["dagit", "pytest"]},
 )
